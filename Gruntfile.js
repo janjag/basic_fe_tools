16cc75262f7c242af96c3d3f8eca33a5ec94aef8
Jan Jagieła <janjag@humanistcanco.de>
2018-12-16 15:43:44 +0100

    basic setup

---
+++ Gruntfile.js
@@ -0,0 +1,92 @@
+const sass = require('node-sass');
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        "dist/*.css",
+                        "dist/*.js",
+                        "*.html"
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: "./",
+                    ui: {
+                        port: 8889,
+                        weinre: {
+                            port: 9999
+                        }
+                    },
+                    port: 8888
+                }
+            }
+        },
+        postcss: {
+            options: {
+                map: true,
+                processors: [
+                    require("autoprefixer")({
+                        browsers: ["last 2 versions", "ie 11"]
+                    })
+                ]
+            },
+            dist: {
+                src: "dist/*.css"
+            }
+        },
+        sass: {
+            options: {
+                sourceMap: true,
+                implementation: sass,
+                outputStyle: "compressed"
+            },
+            dist: {
+                files: {
+                    "dist/main.min.css": "dev/scss/main.scss"
+                }
+            }
+        },
+        uglify: {
+            options: {
+                sourceMap: true,
+                compress: {
+                    drop_console: true
+                },
+                mangle: {
+                    reserved: ["jQuery", "Backbone"]
+                }
+            },
+            prod: {
+                files: {
+                    "dist/app.min.js": ["dev/js/app.js"],
+                    "dist/app-functions.min.js": ["dev/js/app-functions.js"]
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: "dev/**/*.scss",
+                tasks: ["sass", "postcss:dist"]
+            },
+            js: {
+                files: "dev/**/*.js",
+                tasks: ["build-js"]
+            }
+        }
+    });
+
+
+    grunt.loadNpmTasks("grunt-browser-sync");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-postcss");
+    grunt.loadNpmTasks("grunt-sass");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+
+    grunt.registerTask("default", ["browserSync", "watch"]);
+    grunt.registerTask("build-css", ["sass", "postcss:dist"]);
+    grunt.registerTask("build-js", ["uglify"]);
+}; 
---
+++ README.md
@@ -0,0 +1,41 @@
+# Basic front-end tools setup / Podstawowa konfiguracja narzędzi do front-endu
+
+## Basic front-end tools setup.
+### Requires:
+- node.js - download from nodejs.org (LTS recommended)
+
+### Contains:
+- Grunt task runer - requires ground-cli global installation -> `npm install -g grunt-cli`,
+task configuration is in the `Gruntfile.js` file
+- BrowserSync - local server for synchronous testing of websites / web applications available with automatic refresh of views
+- node-sass - a tool for compiling `.scss` files to` .css`
+- PostCSS - a tool for transforming css files, mainly used for adding prefixes to the browser versions of the browser
+- Uglify - a tool for obfuscating javascript code
+
+
+## Recommended editors:
+- Visual Studio Code,
+- SublimeText 3
+
+## Additional tools:
+- ConEmu - overlay on the system console for Windows systems
+
+## Podstawowe środowisko do prac front-endowych.
+### Wymaga:
+- node.js do pobrania z nodejs.org (zalecana wersja LTS)
+
+### Zawiera: 
+- Grunt task runer - do działania wymaga globalnej instalacji grunt-cli -> `npm install -g grunt-cli`,
+konfiguracja zadań jest w pliku `Gruntfile.js`
+- BrowserSync - lokalny server do synchronicznego testowania stron www/aplikacji webowych dostępny z automatycznym odświeżaniem widoków
+- node-sass - narzędzie do kompilacji plików `.scss` do `.css`
+- PostCSS - narzędzie do transformacji plików css, głównie używany do dopisywania prefixów dla strszych wersji przeglądarek
+- Uglify - narzędzie do zaciemniania kodu javascript
+
+
+## Polecane edytory:
+- Visual Studio Code,
+- SublimeText 3
+
+## Dodatkowe narzędzia:
+- ConEmu - nakładka na systemową konsolę dla systemów Windows
